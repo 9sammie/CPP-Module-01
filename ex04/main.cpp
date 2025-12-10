e8fd2ec13af4953ca70b0b2e65443dc3ab43c6b8
9sammie <ballet.mathis@gmail.com>
2025-12-10 11:11:12 +0100

    ex04 working

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: maballet <maballet@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/08 21:58:10 by maballet          #+#    #+#             */
-/*   Updated: 2025/12/09 00:45:26 by maballet         ###   ########lyon.fr   */
+/*   Updated: 2025/12/10 10:59:44 by maballet         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,12 @@ bool	argCheck(std::string filename, std::string s1) {
 
 	if (s1.empty())
 	{
-		std::cerr << "Error: s1 cannot be empty" << std::endl;
+		std::cout << "Error: s1 cannot be empty" << std::endl;
 		return EXIT_FAILURE;
 	}
 	if (filename.empty())
 	{
-		std::cerr << "Error: filename cannot be empty" << std::endl;
+		std::cout << "Error: filename cannot be empty" << std::endl;
 		return EXIT_FAILURE;
 	}
 	return EXIT_SUCCESS;
@@ -31,7 +31,7 @@ int main(int argc, char **argv) {
 
 	if (argc != 4)
 	{
-		std::cerr << "Error: Exactly 3 arguments expected in this order: <filename> <s1> <s2>" << std::endl;
+		std::cout << "Error: Exactly 3 arguments expected in this order: <filename> <s1> <s2>" << std::endl;
 		return EXIT_FAILURE;
 	}
 
@@ -44,7 +44,7 @@ int main(int argc, char **argv) {
 	
 	searchAndReplace tool(filename, s1, s2);
 
-	if (!tool.run())
+	if (tool.run())
 		return EXIT_FAILURE;
 	return EXIT_SUCCESS;
 }
--- ex04/searchAndReplace.cpp
+++ ex04/searchAndReplace.cpp
@@ -6,7 +6,7 @@
 /*   By: maballet <maballet@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/08 23:53:16 by maballet          #+#    #+#             */
-/*   Updated: 2025/12/09 00:34:02 by maballet         ###   ########lyon.fr   */
+/*   Updated: 2025/12/10 11:08:44 by maballet         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,16 +26,23 @@ bool searchAndReplace::run() {
 	std::ifstream input(_filename.c_str());
 	if (!input.is_open())
 	{
-		std::cerr << "Error: error while opening the file" << std::endl;
-		return EXIT_SUCCESS;
+		std::cout << "Error: error while opening the file" << std::endl;
+		return EXIT_FAILURE;
 	}
 	std::string outputName = _filename + ".replace";
 	std::ofstream output(outputName.c_str());
 	std::string line;
 	while (getline(input, line)) {
 
+		size_t OccurrencePos = line.find(this->_search);
+		while (OccurrencePos != (size_t)-1) {
+			
+			line.erase(OccurrencePos, this->_search.length());
+			line.insert(OccurrencePos, this->_replace);
+			OccurrencePos = line.find(this->_search, (OccurrencePos + this->_replace.length()));
+		}
 		output << line;
-		if (!input.eof())
+		if (input.eof())
 			output << std::endl;
 	}
 	return EXIT_SUCCESS;
